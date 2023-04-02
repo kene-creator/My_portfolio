d52cfda178e0c46084b2686d855da27f3d163cf7
Kene <kayyungx@gmail.com>
2023-04-02 13:08:46 +0100

    Add circular progress bar

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,35 @@
 import React from "react";
 import { Html, useProgress } from "@react-three/drei";
+import { CircularProgress, Box, Typography } from "@mui/material";
+
+function CircularProgressWithLabel(props) {
+  return (
+    <Box sx={{ position: "relative", display: "inline-flex" }}>
+      <CircularProgress
+        variant="determinate"
+        {...props}
+        thickness={6}
+        color="secondary"
+      />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography variant="caption" color="white" fontWeight="bold">
+          {`${parseFloat(props.value)}%`}
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
 
 const Loader = () => {
   const { progress } = useProgress();
@@ -14,7 +44,8 @@ const Loader = () => {
           marginTop: 40,
         }}
       >
-        {progress.toFixed(2)}%
+        <CircularProgressWithLabel value={progress.toFixed(0)} />
+        {/* {progress.toFixed(2)} */}
       </p>
     </Html>
   );
