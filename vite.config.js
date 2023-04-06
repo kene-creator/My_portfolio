8411bb49191bf38062fa3f094a5e350cbf642a83
Kene <kayyungx@gmail.com>
2023-04-06 14:19:30 +0100

    Adjust chunk size

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000000, // Set the limit to 1 MB
+    chunkSizeWarningLimit: 2000, // Set the limit to 1 MB
   },
 });
